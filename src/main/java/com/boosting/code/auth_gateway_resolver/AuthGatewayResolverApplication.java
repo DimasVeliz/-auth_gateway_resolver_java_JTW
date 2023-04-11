0febd797da3b3c2d690f51e5f341510bcbf9b484
DimasVeliz <trevor90094@yandex.com>
2023-04-11 21:55:52 +0200

    infrastructure in place

---
+++ src/main/java/com/boosting/code/auth_gateway_resolver/AuthGatewayResolverApplication.java
@@ -0,0 +1,13 @@
+package com.boosting.code.auth_gateway_resolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthGatewayResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthGatewayResolverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boosting/code/auth_gateway_resolver/AuthGatewayResolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boosting.code.auth_gateway_resolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthGatewayResolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
