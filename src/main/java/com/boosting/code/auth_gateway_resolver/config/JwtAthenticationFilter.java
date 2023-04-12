db233ab9e08a7ed37241e4864270bd469724dd0d
DimasVeliz <trevor90094@yandex.com>
2023-04-12 09:51:12 +0200

    Added Jwt filter and Service

---
+++ src/main/java/com/boosting/code/auth_gateway_resolver/config/JwtAthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.boosting.code.auth_gateway_resolver.config;
+
+import com.boosting.code.auth_gateway_resolver.repositories.TokenRepository;
+import com.boosting.code.auth_gateway_resolver.services.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAthenticationFilter extends OncePerRequestFilter {
+    private static final CharSequence AUTH_CONTROLLER_PATH = "/api/v1/auth";
+    private static final int LENGTH_BEARER_PREFIX = 7;
+    private JwtService jwtService;
+    private UserDetailsService userDetailsService;
+    private TokenRepository tokenRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        if(request.getServletPath().contains(AUTH_CONTROLLER_PATH)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if(authHeader==null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        jwt=authHeader.substring(LENGTH_BEARER_PREFIX);
+        userEmail = jwtService.extractUsername(jwt);
+        if(userEmail!=null  && SecurityContextHolder.getContext().getAuthentication()==null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            var isTokenValid = this.tokenRepository.findByToken(jwt)
+                    .map(t -> !t.isExpired() && !t.isRevoked())
+                    .orElse(false);
+            if (jwtService.isTokenValid(jwt, userDetails) && isTokenValid) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/boosting/code/auth_gateway_resolver/entities/TokenType.java
@@ -0,0 +1,5 @@
+package com.boosting.code.auth_gateway_resolver.entities;
+
+public enum TokenType {
+    BEARER
+}
