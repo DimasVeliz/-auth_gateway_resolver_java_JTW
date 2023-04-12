a5412a62214c98ad934b3236ee55c83ceb9d6205
DimasVeliz <trevor90094@yandex.com>
2023-04-12 18:10:50 +0200

    App Running properly, SQL dialect changed to MYSQL instead of postgres, fixed mixed anotations

--- src/main/java/com/boosting/code/auth_gateway_resolver/config/JwtAuthenticationFilter.java
+++ src/main/java/com/boosting/code/auth_gateway_resolver/config/JwtAuthenticationFilter.java
@@ -6,15 +6,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final CharSequence AUTH_CONTROLLER_PATH = "/api/v1/auth";
     private static final int LENGTH_BEARER_PREFIX = 7;
--- src/main/java/com/boosting/code/auth_gateway_resolver/entities/Token.java
+++ src/main/java/com/boosting/code/auth_gateway_resolver/entities/Token.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "_token")
 public class Token {
 
     @Id
