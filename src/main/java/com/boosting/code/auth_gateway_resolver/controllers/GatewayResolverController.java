603ce49a7c9eb0927d69c63a429cc7610ba49f38
DimasVeliz <trevor90094@yandex.com>
2023-04-14 15:23:01 +0200

    Cors added

--- src/main/java/com/boosting/code/auth_gateway_resolver/controllers/GatewayResolverController.java
+++ src/main/java/com/boosting/code/auth_gateway_resolver/controllers/GatewayResolverController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import static com.boosting.code.auth_gateway_resolver.utilities.Utils.generateTrackingUUID;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RequestMapping("/api/v1/data")
 @RequiredArgsConstructor
 public class GatewayResolverController {
